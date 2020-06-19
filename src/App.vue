8fca1e2249ff048b723ece029fe31adce7e64ac4
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-06-19 16:52:50 +0900

    SettingVueRouter

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
+      <router-link to="/rails">Rails</router-link>
+      <router-link to="/vue">Vue</router-link>
     </div>
     <router-view/>
   </div>
