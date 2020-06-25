f4a52c354ad49d2fbb2386d3f9cbd1358d5a26f9
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-06-25 13:29:20 +0900

    [create:]VuePage

---
+++ src/components/Vue.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="toppage-contents-area">
+    <div class="toppage-contents__section-title">Vue.js</div>
+      <div class="appcontents-parent">
+        <div class="appcontents-child">
+          <div class="app-leftbox">
+            <div class="app-image">
+              <img class="lsp-gif" src="@/assets/vue-port.png"/>
+            </div>
+          </div>
+          <div class="app-rightbox">
+            <div class="app-title">
+              <p>KOIDE SHOGO PORTFOLIO</p>
+            </div>
+            <div class="app-overreview">
+               <strong><p>アプリ概要</p></strong>
+               <p>これまでの学習のまとめをポートフォリをとしてまとめました。</p>
+            </div>
+            <div class="app-comment">
+               <strong><p>コメント / ポイント</p></strong>
+               <p>SPAを作成してみたく、Vue, Vuex, Vue Routerを使用してポートフォリオを作成致しました。</p>
+            </div>
+            <div class="app-git">
+               <strong><p>Git hub レポジトリ</p></strong>
+               <a href="https://github.com/koideshogo/Vue-Portfolio">https://github.com/koideshogo/Vue-Portfolio</a>
+            </div>
+            <div class="app-tech">
+               <strong><p>使用技術</p></strong>
+               <p>Vue.js + Vuex + VueRouter +Firebase</p>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<style>
+/* Railsのcomponentsに記述 */
+</style>
