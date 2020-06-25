d04b829a7f72ee8bf1b1728cde16a1bab7e6b9cf
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-06-25 13:31:17 +0900

    [create:]Header

---
+++ src/components/Header.vue
@@ -0,0 +1,78 @@
+<template>
+  <header class="header">
+    <ul class="hero-grobalnav__list">
+      <li><router-link class="hero-grobalnav__item" to="/" >HOME</router-link></li>
+      <li><router-link class="hero-grobalnav__item" to="/about">ABOUT ME</router-link></li>
+      <li><router-link class="hero-grobalnav__item" to="/skills">SKILL SETS</router-link></li>
+      <li><router-link class="hero-grobalnav__item" to="/rails">Ruby on Rails</router-link></li>
+      <li><router-link class="hero-grobalnav__item" to="/vue">Vue.js</router-link></li>
+      <li><router-link class="hero-grobalnav__item" to="/github">Git hub</router-link></li>
+      <li><router-link class="hero-grobalnav__item" to="/contact">CONTACT</router-link></li>
+    </ul>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'Header'
+}
+
+</script>
+
+
+<style>
+.title-header {
+  position: fixed;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  top: 4vw;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  -webkit-box-pack: justify;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  padding: 0 2.7rem 0 5.3rem;
+  z-index: 12;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+a.router-link-exact-active{
+  text-decoration: none;
+  color: black;
+}
+
+
+.hero-grobalnav__list {
+  left: 0;
+  right: 0;
+  top: 5%;
+  display: flex;
+  flex-wrap: wrap;
+  margin: auto;
+  position: absolute;
+  list-style: none;
+  justify-content: space-evenly;
+  z-index: 99;
+}
+
+.hero-grobalnav__item {
+  color: black;
+  flex-wrap: wrap;
+  margin: auto;
+  text-decoration: none;
+}
+
+</style>
