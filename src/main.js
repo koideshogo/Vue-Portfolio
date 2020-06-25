77b02781b36e5d2158e1ec6a8c13af0af6c29d4b
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-06-25 13:29:53 +0900

    [add:]処理追加

--- src/main.js
+++ src/main.js
@@ -1,18 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import firebase from 'firebase'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/router.js';
+import store from './store';
 
-const config = {
-  apiKey: "AIzaSyBE39cguJcLpFJxmJSUfV2rQtfrcqE6qm0",
-  authDomain: "test-app-d6e4b.firebaseapp.com",
-  databaseURL: "https://test-app-d6e4b.firebaseio.com",
-  projectId: "test-app-d6e4b",
-  storageBucket: "test-app-d6e4b.appspot.com",
-  messagingSenderId: "712383253433"
-};
-firebase.initializeApp(config)
 Vue.config.productionTip = false
 
 new Vue({
