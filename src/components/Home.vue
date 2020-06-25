261e97a52af4ab438c4e89f790bbc02ed87b450b
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-06-25 13:31:50 +0900

    [create:]HomePage

---
+++ src/components/Home.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="hero-contens">
+    <div class="hero-contents-title">
+      <h1 class="title">KOIDE SHOGO PORTFOLIO</h1>
+    </div>
+    <img class="TopImage" src="@/assets/KOIDESHOGO.jpg" width="100%"/>
+    <vue-typer class="hero-typer"
+      :text='["お忙しいところ閲覧いただきましてありがとうございます。"]'
+      :repeat='0'
+      :shuffle='false'
+      initial-action='typing'
+      :pre-type-delay='800'
+      :type-delay='70'
+      :pre-erase-delay='2000'
+      :erase-delay='250'
+      erase-style='select-all'
+      :erase-on-complete='false'
+      caret-animation='blink'
+    ></vue-typer>
+</div>
+</template>
+
+
+<script>
+import { VueTyper } from 'vue-typer'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: "Home",
+  components: {
+     VueTyper
+  }
+}
+
+
+</script>
+
+<style scoped>
+.hero-contens{
+  top: 0;
+  height: 100vh;
+  position: relative;
+  left: 0;
+  right: 0;
+  width: 65%;
+  margin: auto;
+}
+  .hero-contens-title {
+
+
+  }
+
+  h1 {
+    font-size: 2.723vw;
+    line-height: 1;
+    margin: .67em 0 0;
+    position: absolute;
+    top: 32%;
+    z-index: 98;
+    left: 18%;
+
+  }
+  .TopImage {
+    position: absolute;
+    top: 25%;
+    right: 0; 
+    left: 0;
+  }
+  
+  .hero-typer {
+    position: absolute;
+    left: 25%;
+    top: 45%;
+    margin: auto;
+    color: black;
+    z-index: 10;
+    font-family: Geneva,Tahoma,Verdana,sans-serif;
+    letter-spacing: .05em
+  }
+</style>
