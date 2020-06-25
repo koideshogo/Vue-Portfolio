9eab1f2fcf694967864ae13daf9c7b486acb6c98
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-06-25 13:28:21 +0900

    [create:]GitPage

---
+++ src/components/Git.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="toppage-contents-area">
+      <div class="toppage-contents__section-title">Git hub</div>
+      <p>現在準備中です。axios　APIを使用して表示予定です。</p>
+    </div>
+</template>
+
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Git'
+}
+
+</script>
+
+<style scoped>
+  p {
+    text-align: center;
+  }
+</style>
