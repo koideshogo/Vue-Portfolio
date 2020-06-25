2cc907bcfbc6fc9e3d1e9e842873f2f3aaa511a4
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-06-25 13:27:41 +0900

    [create:]ContactPage

---
+++ src/components/Contact.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="toppage-contents-area">
+      <div class="toppage-contents__section-title">ABOUT ME</div>
+      <div class="contact-parentcontents">
+        <div class="contact-childcontents">
+          <strong><p>何かご連絡あれば下記までよろしくお願い申し上げます。</p></strong>
+          <p>G-mail</p>
+          <p><a href="mailto:shogo10261026@gmail.com">shogo10261026@gmail.com</a></p>
+          <p>wantedly</p>
+          <p><a href="https://www.wantedly.com/users/116247291">https://www.wantedly.com/users/116247291</a></p>
+        </div>
+        </div>
+      </div>
+    </div>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+
+
+export default {
+  name: 'Contact'
+}
+
+</script>
+
+<style>
+.contact-parentcontents {
+    background: #F5F5F5;
+    width: 80%;
+    padding: 0.5em 1em;
+    margin: auto;
+    border-bottom: solid 6px ;
+}
+
+</style>
